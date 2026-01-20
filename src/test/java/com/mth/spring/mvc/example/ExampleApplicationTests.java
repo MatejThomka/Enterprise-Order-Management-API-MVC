17cba23c0cf2266ee35bab12f95ead89f111b5f6
Matej Thomka <matej.thomka.eleinst@gmail.com>
2026-01-20 10:33:25 +0100

    initialization of spring boot project

---
+++ src/test/java/com/mth/spring/mvc/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mth.spring.mvc.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

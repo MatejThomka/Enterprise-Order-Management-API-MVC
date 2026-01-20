eac31adf13585a884315b9cbdadb13dd0aed50e1
Matej Thomka <matej.thomka.eleinst@gmail.com>
2026-01-21 00:53:39 +0100

    change from record to class, define all fields, setters, getters

--- src/main/java/com/mth/spring/mvc/ordermanagement/model/Item.java
+++ src/main/java/com/mth/spring/mvc/ordermanagement/model/Item.java
@@ -13,9 +13,65 @@ import org.springframework.data.annotation.Id;
 
 @Entity
 @Table(name = "items")
-public record Item(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id,
-    @Column(nullable = false) String name,
-    @Column(nullable = false) String description,
-    BigDecimal price,
-    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "order_id") Order order) {}
+public class Item {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+
+  @Column(nullable = false)
+  String name;
+
+  @Column(nullable = false)
+  String description;
+
+  BigDecimal price;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "order_id")
+  Order order;
+
+  protected Item() {}
+
+  public Item(String name, String description, BigDecimal price, Order order) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.order = order;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String description() {
+    return description;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public Order getOrder() {
+    return order;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  public void setOrder(Order order) {
+    this.order = order;
+  }
+}

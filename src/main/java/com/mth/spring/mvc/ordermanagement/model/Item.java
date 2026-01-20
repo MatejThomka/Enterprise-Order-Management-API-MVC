27c747a661535ba941b754992d1f97900ec4ea6e
Matej Thomka <matej.thomka.eleinst@gmail.com>
2026-01-20 22:22:37 +0100

    add fields to Item

--- src/main/java/com/mth/spring/mvc/ordermanagement/model/Item.java
+++ src/main/java/com/mth/spring/mvc/ordermanagement/model/Item.java
@@ -1,5 +1,21 @@
 package com.mth.spring.mvc.ordermanagement.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
 import org.springframework.data.annotation.Id;
 
-public record Item(@Id Long id, String name, String description) {}
+@Entity
+@Table(name = "items")
+public record Item(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id,
+    @Column(nullable = false) String name,
+    @Column(nullable = false) String description,
+    BigDecimal price,
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "order_id") Order order) {}

cd8adbd30aee2324d73e053dbb5be9caad4aadfe
Matej Thomka <matej.thomka.eleinst@gmail.com>
2026-01-20 22:23:19 +0100

    add annotation

--- src/main/java/com/mth/spring/mvc/ordermanagement/repository/ItemRepository.java
+++ src/main/java/com/mth/spring/mvc/ordermanagement/repository/ItemRepository.java
@@ -2,5 +2,7 @@ package com.mth.spring.mvc.ordermanagement.repository;
 
 import com.mth.spring.mvc.ordermanagement.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {}

de1fd20f070c869e8697fc90d668e5b420a4402b
Matej Thomka <matej.thomka.eleinst@gmail.com>
2026-01-21 11:54:19 +0100

    update Item repository with Pageable function

--- src/main/java/com/mth/spring/mvc/ordermanagement/repository/ItemRepository.java
+++ src/main/java/com/mth/spring/mvc/ordermanagement/repository/ItemRepository.java
@@ -1,8 +1,14 @@
 package com.mth.spring.mvc.ordermanagement.repository;
 
 import com.mth.spring.mvc.ordermanagement.model.Item;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemRepository extends JpaRepository<Item, Long> {}
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    Page<Item> findByOrderId(Long orederId, Pageable pageable);
+}

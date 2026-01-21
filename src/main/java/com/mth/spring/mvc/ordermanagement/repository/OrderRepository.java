e66b77bc23b80e9775fc17cc9e0a65e761945605
Matej Thomka <matej.thomka.eleinst@gmail.com>
2026-01-21 11:54:41 +0100

    update Order repository with basic functions and pageable

--- src/main/java/com/mth/spring/mvc/ordermanagement/repository/OrderRepository.java
+++ src/main/java/com/mth/spring/mvc/ordermanagement/repository/OrderRepository.java
@@ -1,8 +1,19 @@
 package com.mth.spring.mvc.ordermanagement.repository;
 
 import com.mth.spring.mvc.ordermanagement.model.Order;
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+  Optional<Order> findByOrderNumber(String orderNumber);
+
+  boolean existByOrderNumber(String orderNumber);
+
+  Page<Order> findByCustomerId(Long customerId, Pageable pageable);
+}

0419c338a08a0a135cd859a4089d7b5bdfbcc09f
Matej Thomka <matej.thomka.eleinst@gmail.com>
2026-01-25 21:10:55 +0100

    add doc and fields

--- src/main/java/com/mth/spring/mvc/ordermanagement/dto/request/CreateOrderRequest.java
+++ src/main/java/com/mth/spring/mvc/ordermanagement/dto/request/CreateOrderRequest.java
@@ -1,5 +1,11 @@
 package com.mth.spring.mvc.ordermanagement.dto.request;
 
-public record CreateOrderRequest() {
-    
-}
+import java.util.List;
+
+/**
+ * Data transfer object for creating a new order
+ *
+ * @param customer the customer information for the new order
+ * @param items the list of items to be included in the new order
+ */
+public record CreateOrderRequest(CreateCustomerRequest customer, List<CreateItemRequest> items) {}

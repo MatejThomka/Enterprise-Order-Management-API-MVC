8778ea2f4518990b357e7a0f8f163473d2192b3d
Matej Thomka <matej.thomka.eleinst@gmail.com>
2026-01-25 14:44:41 +0100

    add doc, add methods for mapping toEntity and updateEntity

--- src/main/java/com/mth/spring/mvc/ordermanagement/mapper/CustomerMapper.java
+++ src/main/java/com/mth/spring/mvc/ordermanagement/mapper/CustomerMapper.java
@@ -1,11 +1,20 @@
 package com.mth.spring.mvc.ordermanagement.mapper;
 
+import com.mth.spring.mvc.ordermanagement.dto.request.CreateCustomerRequest;
+import com.mth.spring.mvc.ordermanagement.dto.request.UpdateCustomerRequest;
 import com.mth.spring.mvc.ordermanagement.dto.response.CustomerDto;
 import com.mth.spring.mvc.ordermanagement.dto.response.CustomerSummaryDto;
 import com.mth.spring.mvc.ordermanagement.model.Customer;
 
+/** CustomerMapper class for mapping Customer entity from and into dto's */
 public class CustomerMapper {
 
+  /**
+   * Converts a Customer entity to a CustomerDto.
+   *
+   * @param customer the Customer entity to convert
+   * @return the corresponding CustomerDto
+   */
   public static CustomerDto toCustomerDto(Customer customer) {
     if (customer == null) {
       return null;
@@ -18,11 +27,40 @@ public class CustomerMapper {
         customer.getPhoneNumber());
   }
 
+  /**
+   * Converts a Customer entity to a CustomerSummaryDto.
+   *
+   * @param customer the Customer entity to convert
+   * @return the corresponding CustomerSummaryDto
+   */
   public static CustomerSummaryDto toCustomerSummaryDto(Customer customer) {
     if (customer == null) {
       return null;
     }
     return new CustomerSummaryDto(
         customer.getId(), customer.getFirstName(), customer.getLastName(), customer.getEmail());
+  }
+
+  /**
+   * Converts a CreateCustomerRequest to a Customer entity.
+   *
+   * @param request the CreateCustomerRequest to convert
+   * @return the corresponding Customer entity
+   */
+  public static Customer toEntity(CreateCustomerRequest request) {
+    return new Customer(
+        request.firstName(), request.lastName(), request.email(), request.phoneNumber());
+  }
+
+  /**
+   * Updates a Customer entity with data from an UpdateCustomerRequest.
+   *
+   * @param customer the Customer entity to update
+   * @param request the UpdateCustomerRequest containing the updated data
+   */
+  public static void updateEntity(Customer customer, UpdateCustomerRequest request) {
+    customer.setFirstName(request.firstName());
+    customer.setLastName(request.lastName());
+    customer.setPhoneNumber(request.phoneNumber());
   }
 }

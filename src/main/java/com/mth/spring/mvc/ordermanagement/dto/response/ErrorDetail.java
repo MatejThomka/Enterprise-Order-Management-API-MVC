a58859f0e37c4a38345ec61e57dcdaac904e6276
Matej Thomka <matej.thomka.eleinst@gmail.com>
2026-01-26 23:39:44 +0100

    prepare error detail record

---
+++ src/main/java/com/mth/spring/mvc/ordermanagement/dto/response/ErrorDetail.java
@@ -0,0 +1,4 @@
+package com.mth.spring.mvc.ordermanagement.dto.response;
+
+/** Represents error details in an error response. */
+public record ErrorDetail(String errorMessage, String errorCode) {}
